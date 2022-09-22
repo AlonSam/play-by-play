664fb96ece32698e2ead1ba8a1bb3b56b870abb3
Alon <alonsamocha@gmail.com>
2022-09-22 18:19:06 +0300

    Created exceptions.py - Will contain all custom exceptions

---
+++ pbp/exceptions.py
@@ -0,0 +1,22 @@
+class EmptyPossession(BaseException):
+    pass
+
+
+class ReboundNotFound(BaseException):
+    pass
+
+
+class MissedShotNotFound(BaseException):
+    pass
+
+
+class ShotLocationUnknown(BaseException):
+    pass
+
+
+class LineupWithDuplicates(BaseException):
+    pass
+
+
+class InvalidLineupLength(BaseException):
+    pass
