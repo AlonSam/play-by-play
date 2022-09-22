084e473574981a2ade53fa2c445fdadeb498065a
Alon <alonsamocha@gmail.com>
2022-09-22 18:21:21 +0300

    Updated attribute names and types

--- pbp/resources/boxscore/segev_boxscore_item.py
+++ pbp/resources/boxscore/segev_boxscore_item.py
@@ -1,18 +1,31 @@
 KEY_ATTR_MAPPER = {
     "playerId": "player_id",
     "jerseyNumber": "shirt_num",
-    "minutes": "min",
     "plusMinus": "plus_minus",
-    "fb": "fastbreak_pts",
-    "points": "pts",
+    "fb": "fastbreak_points",
     "rate": "pir",
-    "sc": "second_chance_pts",
-    "Points_from_turnovers": "turnover_pts",
+    "sc": "second_chance_points",
+    "Points_from_turnovers": "turnover_points",
     "Points_in_the_paint": "points_in_the_paint",
-    "Second_chance_points": "second_chance_pts",
+    "Second_chance_points": "second_chance_points",
+    'ast': 'assists',
+    'to': 'turnovers',
+    'blk': 'blocks',
+    'blk_against': 'blocked',
+    'f': 'fouls',
+    'f_drawn': 'fouls_drawn',
+    'stl': 'steals',
+    'dunk': 'dunks',
+    'dfl': 'deflections',
+    'rec': 'recoveries',
+    'reb_o': 'offensive_rebounds',
+    'reb_d': 'defensive_rebounds',
+    'fg_3m': 'FG3M',
+    'fg_2m': 'FG2M',
+    'ft_m': 'FTM'
     }
 
-KEYS = ['team_id', 'game_id', 'ast', 'to', 'stl', 'dunk', 'dfl', 'rec', 'vps']
+KEYS = ['team_id', 'game_id', 'vps', 'points', 'starter', 'minutes']
 
 
 class SegevBoxScoreItem:
@@ -30,23 +43,16 @@ class SegevBoxScoreItem:
 
     def _set_attributes(self, data):
         data = {k: int(v) if isinstance(v, str) and v.isnumeric() else v for k, v in data.items()}
-        data['treb'] = data['reb_o'] + data['reb_d']
-        data['threepa'] = data['fg_3m'] + data['fg_3mis']
-        data['twopa'] = data['fg_2m'] + data['fg_2mis']
-        data['fta'] = data['ft_m'] + data['ft_mis']
-        data['fgm'] = data['fg_2m'] + data['fg_3m']
-        data['fga'] = data['twopa'] + data['threepa']
-        data['twopct'] = self.cal_pct(data['fg_2m'], data['twopa'])
-        data['threepct'] = self.cal_pct(data['fg_3m'], data['threepa'])
-        data['fgpct'] = self.cal_pct(data['fgm'], data['fga'])
-        data['ftpct'] = self.cal_pct(data['ft_m'], data['fta'])
-        self.reb = dict(treb=data['treb'], dreb=data['reb_d'], oreb=data['reb_o'])
-        self.fg = dict(made=data['fgm'], attempted=data['fga'], pct=data['fgpct'])
-        self.two_pt = dict(made=data['fg_2m'], attempted=data['twopa'], pct=data['twopct'])
-        self.three_pt = dict(made=data['fg_3m'], attempted=data['threepa'], pct=data['threepct'])
-        self.ft = dict(made=data['ft_m'], attempted=data['fta'], pct=data['ftpct'])
-        self.fouls = dict(made=data['f'], drawn=data['f_drawn'])
-        self.blocks = dict(made=data['blk'], blocked=data['blk_against'])
+        self.total_rebounds = int(self.offensive_rebounds) + int(self.defensive_rebounds)
+        self.FG3A = int(self.FG3M) + data['fg_3mis']
+        self.FG2A = int(self.FG2M) + data['fg_2mis']
+        self.FTA = int(self.FTM) + data['ft_mis']
+        self.FGM = int(self.FG2M) + int(self.FG3M)
+        self.FGA = int(self.FG2A) + int(self.FG3A)
+        self.FG2PCT = self.cal_pct(int(self.FG2M), int(self.FG2A))
+        self.FG3PCT = self.cal_pct(int(self.FG3M), int(self.FG3A))
+        self.FGPCT = self.cal_pct(int(self.FGM), int(self.FGA))
+        self.FTPCT = self.cal_pct(int(self.FTM), int(self.FTA))
 
     @staticmethod
     def cal_pct(made: int, attempted: int) -> float:
